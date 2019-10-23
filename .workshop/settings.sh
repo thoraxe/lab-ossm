54bf5932991a577119380c0b070ec276270aef68
Erik M Jacobs <erikmjacobs@gmail.com>
2019-10-23 12:04:33 -0400

    look for image tagged production

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,5 @@
 WORKSHOP_NAME=lab-ossm
-WORKSHOP_IMAGE=quay.io/thoraxe/lab-ossm:latest
+WORKSHOP_IMAGE=quay.io/thoraxe/lab-ossm:production
 RESOURCE_BUDGET=medium
 MAX_SESSION_AGE=3600
 IDLE_TIMEOUT=300
